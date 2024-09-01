ba87850683a6fd7126bd267045c232dc85ef517f
IMisbahK <m.misbahkhursheed@gmail.com>
2024-09-01 20:46:38 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ export default function App(){
  return(
   <div className="calculator-grid">
     <div className="output">
-      <div className="previous-operand"></div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">123,123 * </div>
+      <div className="current-operand">5,333</div>
     </div>
     <button className="span-two">AC</button>
     <button>DEL</button>
